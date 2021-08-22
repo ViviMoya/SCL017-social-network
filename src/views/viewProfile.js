6b3e6b0c32f89231d4339351439a641f81bbcddf
ViviMoya <v.moyacapitan@gmail.com>
2021-08-22 16:19:53 -0400

    se prepara rama master para hacer merge de sprint7

--- src/views/viewProfile.js
+++
@@ -1,8 +0,0 @@
-export const viewProfile = () => {
-  const containerProfileTemplate = document.createElement('div');
-  containerProfileTemplate.className = 'container__profile-template';
-  const profileTemplate = `
-    <div class="view__profile">profile</div>`;
-  containerProfileTemplate.innerHTML = profileTemplate;
-  return containerProfileTemplate;
-};
